5eb6c9ff4dfc2c2663cbe2af25903236190af9cf
unknown <ali.khosro72@gmail.com>
2019-03-07 14:18:58 +0330

    Nav Bar && change pages with click && keep-alive

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app" class="container">
+    <button @click="selectedComponent = 'appQuote'">Quote</button>
+    <button @click="selectedComponent = 'appAuthor'">Author</button>
+    <button @click="selectedComponent = 'appNew'">New</button>
+    <hr>
+    <p>{{ selectedComponent }}</p>
+    <keep-alive>
+      <component :is="selectedComponent">
+        <p>Default Content</p>
+      </component>
+    </keep-alive>
+<!--    <app-quote>
+      <h2 slot="title">{{ quoteTitle }}</h2>
+      <p>A wonderful quote!</p>
+    </app-quote>-->
+  </div>
+</template>
+
+<script>
+  import Quote from './components/Qoute'
+  import New from './components/New'
+  import Author from './components/Author'
+export default {
+      data : function(){
+        return{
+            quoteTitle : 'The Quote',
+            selectedComponent : 'appQuote'
+        }
+      },
+    components: {
+        appQuote : Quote,
+        appNew : New,
+        appAuthor : Author
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
